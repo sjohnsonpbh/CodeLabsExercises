2f1b313500aaa3ebebb33b076e1cf2558b8e7ad8
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-02-21 16:27:16 -0600

    Reorganize Class2 and Class3 folders

---
+++ Class3/exercise1/main.js
@@ -0,0 +1,9 @@
+// - [ ] Create a function
+// - [ ] Have that function take in an argument
+// - [ ] Print a statement that uses the variable along with some other text
+
+
+function saySomething(message) {
+    console.log(`Someone said ${message}`);
+}
+saySomething("Yo!");
