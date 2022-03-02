711a5186f21e0d06e26459d11c0fa4f1213851ca
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-02 12:35:55 -0600

    Class 5 names almost working

--- Class5/app.js
+++ Class5/app.js
@@ -1,29 +1,57 @@
 // create an array to store data
 // get user and password, 
-// add usuer and password to array
+// add user and password to array
 // display the list
 
 
-let userDataList = [];
+// let userDataList = [];
+
+// this is called at end of saveLocal
+function showLocalStorage() {
+
+    // get showSubmission
+    getdivclass = document.getElementById('show-sub');
+
+    console.log(getdivclass);
+
+    showSubmission.innerHTML = displayHtml
 
 
-function saveLocal() {
+}
+
+// this is called by onClick
+function saveLocal(userDataList = []) {
 
     // console.log(e);
 
     let myUser = document.getElementById('username').value;
     let myPass = document.getElementById('password').value;
 
-    console.log(myUser);
-    console.log(myPass);
+    // console.log(myUser);
+    // console.log(myPass);
 
     // add to array
     userDataList.push(myUser, myPass);
 
     // show the array
-    console.dir(userDataList);
+    // console.dir(userDataList);
     // commit to local storage
     localStorage.setItem('userDataList', JSON.stringify(userDataList));
+
+    // build a display div
+
+    let displayHtml = `
+        <div class="my-user">${myUser}</div>
+        <div class="my-pass">${myPass}</div>
+        `;
+
+    // console.log(displayHtml);
+
+    // showLocalStorage.innerHTML = displayHtml;
+    getdivclass = document.querySelector('show-sub');
+
+    console.log(getdivclass);
+
 
 }
 
