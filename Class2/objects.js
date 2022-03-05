5198d770b8b38ed32b215e30da791ec30e780bc1
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-05 12:25:41 -0600

    Studying Objects and functions again

--- Class2/objects.js
+++ Class2/objects.js
@@ -1,28 +1,10 @@
 // everything is an object in javascript, even a string, so you can run a method on the object - cool
 
 
-const hero = {
-    name: 'Bruce Wayne',
-    alias: 'Batman',
-    catchphrase: 'To the Batcave!',
-    speak: function() {
-        return 'Attention! ' + this.catchphrase;
-    },
-    attack: function(sound) {
-        // return '(punches bad guy) ' + sound
-        return `(punches bad guy) ${sound}`;
-    }
-};
-
 // access an object's property
-const thingToLookFor = 'alias';
 
-console.log(hero.name); // Bruce Wayne
-console.log(hero['name']); // Bruce Wayne
-console.log(hero[thingToLookFor]); // Batman
 
 // accessing methods (functions)
-console.log(hero.speak());
-console.log(hero.attack('POWWWWW !'));
+
 
 // 'this is my string'.toUpperCase();
