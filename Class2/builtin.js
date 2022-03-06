2ff34335136e1940f88b08f6e9061001d4f463d7
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-06 13:38:41 -0600

    Reviewing BetterDev objects

--- Class2/builtin.js
+++ Class2/builtin.js
@@ -1,58 +1,64 @@
-        // String and Boolean are both built-in Objects
+const first = "this is a string";
+const second = String("this is a string");
+
+// Primitives vs Objects
+console.groupCollapsed("Primitives vs Objects");
+console.log(first === second); // true
+console.log(
+    typeof first,
+    typeof String("this is a string"),
+    typeof new String()
+);
+console.groupEnd();
+// END Primitives vs Objects
 
 
-        const first = 'this is a string';
-        const second = String('this is a string');
-      // console.log('this is my string'.toUpperCase());
-      // console.log(first === second);
-      
-      console.groupCollapsed('Primitives vs Objects');
-          console.log(first === second); // true
-          console.log(
-              typeof first,
-              typeof String('this is a string'),
-              typeof new String()
-          );
-      console.groupEnd();
+// strings
+const sentence = "this is my sentence";
 
-      // strings
-      const sentence = 'this is my sentence';
+console.groupCollapsed("String");
+console.log(first.toUpperCase()); // THIS IS A STRING
+console.log(second.toLowerCase()); // this is a string
+console.log(sentence.startsWith("this is")); // true
+console.log("scott ".repeat(10));
 
-      console.groupCollapsed('String');
-          console.log(first.toUpperCase()); // THIS IS A STRING
-          console.log(second.toLowerCase()); // this is a string
-          console.log(sentence.startsWith('this is')); // true
-          console.log('X '.repeat(20));
-          console.log('look at me goo     '.trim());
-      console.groupEnd();
-
-      console.groupCollapsed('Numbers and Math');
-          console.log(1..toString()); // 1
-          console.log((1).toString()); // 1
-          console.log(Number.isInteger(5)); // true
-          console.log(Number.isInteger(5.43)); // false
-          console.log((1.562342).toFixed(2)); // 1.56
-          console.log(
-              Math.random().toFixed(2), // random
-              Math.ceil(1.56), // 2
-              Math.floor(6.3) // 6
-          );
-     console.groupEnd();
-
-      // arrays
-      const myArr = ['chris', 'nick', 'holly'];
-      console.group('Arrays');
-          console.log(myArr.length); // 3
-          console.log(myArr.join('-')); // chris-nick-holly
-          console.log(myArr.push('ado'), myArr); //4 (4) ['chris', 'nick', 'holly', 'ado']
-          console.log(myArr.pop(), myArr); // ado (3) ['chris', 'nick', 'holly']
-
-          // myArr.forEach(function(user) {
-          //     console.log(user);
-          // });
-
-          // same as above, but with arrow notation
-          myArr.forEach((user) => console.log(user));
+console.log("look at me go      ".trim());
+console.groupEnd();
+// END strings
 
 
-      console.groupEnd();
+// numbers and math
+console.groupCollapsed("Numbers and Math");
+
+console.log((1).toString()); // 1
+console.log((1).toString()); // 1
+console.log(Number.isInteger(5)); // true
+console.log(Number.isInteger(5.432)); // false
+console.log((1.5452116).toFixed(2)); // 1.55
+console.log(
+    Math.random().toFixed(4), // 0.9975
+    Math.ceil(1.56), // 2
+    Math.floor(6.3), // 6
+);
+console.groupEnd();
+
+// END numbers and math
+
+// arrays
+const myArr = ['chris', 'nick', 'holly'];
+console.group('Arrays');
+console.log(myArr.length); // 3
+console.log(myArr.join('-')); // chris-nick-holly -- pass in something to seperate array elements 
+console.log(myArr.push('ado'), myArr)
+console.log(myArr.pop(), myArr); // do not have to pass anything in, just removes last array item 
+
+// remember me arrow function example below
+
+// myArr.forEach(function (user) {
+//     console.log(user);
+// });
+
+myArr.forEach((user) => console.log(user));
+console.groupEnd();
+
+// END arrays
