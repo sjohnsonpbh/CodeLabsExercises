cb60fdf16d5f64c96f6f4d9edd7554e891e9b2cd
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-07 13:49:46 -0600

    Monday class 7 push

---
+++ Class7/exercise1/scripts.js
@@ -0,0 +1,10 @@
+console.log("The scripts.js file is loaded.");
+
+//  +++++++++++++++++++ SET DOM VARIABLES   +++++++++++++++++++
+
+//  +++++++++++++++++++ FUNCTIONS +++++++++++++++++++ 
+
+//  +++++++++++++++++++ EVENT LISTENERS +++++++++++++++++++
+
+//  +++++++++++++++++++ FUNCTION CALLS +++++++++++++++++++
+
---
+++ mosh/ifelse/scripts.js
@@ -0,0 +1,21 @@
+
+
+//  +++++++++++++++++++ SET DOM VARIABLES   +++++++++++++++++++
+
+let hour = 5;
+
+if (hour >= 6 && hour < 12)
+    console.log('Good morning');
+else if (hour >= 12 && hour <= 18)
+    console.log('Good afternoon');
+else
+    console.log('Good evening');
+
+
+
+//  +++++++++++++++++++ FUNCTIONS +++++++++++++++++++ 
+
+//  +++++++++++++++++++ EVENT LISTENERS +++++++++++++++++++
+
+//  +++++++++++++++++++ FUNCTION CALLS +++++++++++++++++++
+
