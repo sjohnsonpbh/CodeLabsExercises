564d9c401c9b633458941aab7ce9f2fad90bc5ab
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-14 14:42:33 -0500

    Monday post class push

--- Class8/exercise3/scripts.js
+++ Class8/exercise3/scripts.js
@@ -17,7 +17,8 @@ If all the above conditions are satisfied, return "qualified", else return "disq
 
 You will be given an array of time taken by a candidate to solve a particular question and the total time the candidate takes to complete the interview.
 
-Given an array, in a true condition will always be in the format [very_easy, very_easy, easy, easy, medium, medium, hard, hard].
+Given an array, in a true condition will always be in the format 
+[very_easy, very_easy, easy, easy, medium, medium, hard, hard].
 
 The maximum time to complete the interview includes a buffer time of 20 minutes.
 
@@ -31,25 +32,41 @@ The maximum time to complete the interview includes a buffer time of 20 minutes.
 */
 
 
+
 //  +++++++++++++++++++ GET DOM, SET VARIABLES   +++++++++++++++++++
 
 //  +++++++++++++++++++ FUNCTIONS +++++++++++++++++++ 
 function interview(questionArray, interviewTime) {
-
-
     // total time and number of answers
     if ((interviewTime > 120) || (questionArray.length < 8)) return "disqualified";
 
-    // loop through questionarray
+    // loop through questionArray
     for (var i = 0; i < questionArray.length; i++) {
-        if ((questionArray[0]) > 5);
-        console.log('i:', i)
-        console.log('questionArray[0]:', questionArray[0])
-        return "disqualified";
+
+        if ((i == 0) || (i == 1) && ((questionArray[0] <= 5)) || (questionArray[1] <= 5) {
+            //  very_easy 5 min max
+            console.log('questionArray[0]:', questionArray[0]);
+            return "disqualified";
+        }
+        else if ((questionArray[2]) && ((questionArray[3]) > 10)) {
+            // are easy, 10 min n=max
+            console.log('questionArray[2]:', questionArray[2]);
+            return "disqualified";
+        }
+        else if ((questionArray[4]) && ((questionArray[5]) > 15)) { // medium 15 max
+            console.log('questionArray[4]:', questionArray[5]);
+            return "disqualified";
+        }
+
+        else if ((questionArray[6]) && ((questionArray[7]) > 20)) { // hard 20 max
+            console.log('questionArray[6]:', questionArray[7]);
+            return "disqualified";
+        }
+
     }
     return "qualified";
-
 }
+
 //  +++++++++++++++++++ EVENT LISTENERS +++++++++++++++++++
 
 //  +++++++++++++++++++ FUNCTION CALLS +++++++++++++++++++
