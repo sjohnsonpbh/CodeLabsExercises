bb05f57fc1bd90b8deb995fba1b460c26e73bb62
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-16 08:03:02 -0500

    Update basics.tx

--- Class9/TypescriptEx/basics.ts
+++ Class9/TypescriptEx/basics.ts
@@ -22,24 +22,109 @@ let hobbies: string[];
 
 hobbies = ["Sports", "Cooking"];
 
-let person: {
+// below is an example of using an Alias (Person)
+type Person = {
     name: string;
     age: number;
 };
+
+let person: Person;
 
 person = {
     name: "Max",
     age: 32,
 };
 
-let people: {
-    name: string;
-    age: number;
-}[]; // Note the last bracket tells typescript we are making an array of people
+let people: Person[]; // Note the last bracket tells typescript we are making an array of people
 
 // Function types, parameters
 
 // Type inference, if you give value when delcaring a variable, it can tell this should be a string.
+
+// the below is called a union type, i.e. multiple types
 let course: string | number = "React - The Complete Guide";
 
 course = 12341;
+
+// Functions & types
+
+function add(a: number, b: number) {
+    return a + b; // typescript infers this to be a number, b/c we passed in numbers
+}
+
+// Functions & types
+
+function addNumbers(a: number, b: number) {
+    return a + b;
+}
+
+function printOutput(value: any) {
+    console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+// updatedArray[0].split('');
+
+class Student {
+    // firstName: string;
+    // lastName: string;
+    // age: number;
+    // private courses: string[];
+
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public age: number,
+        private courses: string[]
+    ) {}
+
+    enrol(courseName: string) {
+        this.courses.push(courseName);
+    }
+
+    listCourses() {
+        return this.courses.slice();
+    }
+}
+
+const student = new Student("Max", "Schwarz", 32, ["Angular"]);
+student.enrol("React");
+// student.listCourses(); => Angular, React
+
+// student.courses => Angular, React
+
+interface Human {
+    firstName: string;
+    age: number;
+
+    greet: () => void;
+}
+
+let max: Human;
+
+max = {
+    firstName: "Max",
+    age: 32,
+    greet() {
+        console.log("Hello!");
+    },
+};
+
+class Instructor implements Human {
+    firstName: string;
+    age: number;
+    greet() {
+        console.log("Hello!!!!");
+    }
+}
