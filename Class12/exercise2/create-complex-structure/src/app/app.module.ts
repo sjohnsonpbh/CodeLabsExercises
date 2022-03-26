1f415df11468f73b628baa37126ee67e55cb14ef
Scott Johnson <sjohnsonpbh@yahoo.com>
2022-03-26 15:34:02 -0500

    quitting for the day

--- Class12/exercise2/create-complex-structure/src/app/app.module.ts
+++ Class12/exercise2/create-complex-structure/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { PostsComponent } from './posts/posts.component';
+import { FriendsComponent } from './friends/friends.component';
+import { FriendsListComponent } from './friends-list/friends-list.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    PostsComponent
+    PostsComponent,
+    FriendsComponent,
+    FriendsListComponent,
+    MyProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
