3f4f4ac4088f0570c39c76e478789abce8325175
Scott <scott@Johnson-Macbook-Pro-Grey.local>
2022-03-28 14:38:49 -0500

    Post class push

---
+++ Class13/dynamic-button-event-binding/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'dynamic-button-event-binding';
+  isDisabled: boolean = true;
+}
