44b90f6131b319866bce135bf8a4bd53096f8d5b
Scott Johnson <mastertheclouds@gmail.com>
2022-03-31 08:33:31 -0500

    Thursday am push

---
+++ Class13/exercise9/dynamically-change-content-ngstyle-exercise/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { getCurrencySymbol } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  changeColor: string = 'green';
+}
---
+++ max/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,37 @@
+// remember me -  encapsulation: ViewEncapsulation.None
+//- on line 11, this removed the weird style names in the html
+// for this component, just uses normal <div> and <p> tags -
+
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated, // None, Shadowdom
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+  // remember me - binding a child element to it's parent
+  // here it is binding to the element, we add @Input() to expose it - this is binding to custom properties
+  @Input('srvElement') element: { type: string; name: string; content: string };
+
+  constructor() {
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+}
