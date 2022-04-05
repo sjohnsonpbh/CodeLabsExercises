f83fc5ba4195445b45872a22de11b4a0a0993627
Scott Johnson <mastertheclouds@gmail.com>
2022-04-05 14:50:39 -0500

    Class14 Ex 4 - not completed

--- Class14/exercise4/angular-basic-custom-event-exercise/src/app/navbar/navbar.component.ts
+++ Class14/exercise4/angular-basic-custom-event-exercise/src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  @Output() searchEmitter = new EventEmitter<{ searchEmitter: string }>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSearchClick() {
+    this.searchEmitter.emit(mySearch);
   }
-
 }
