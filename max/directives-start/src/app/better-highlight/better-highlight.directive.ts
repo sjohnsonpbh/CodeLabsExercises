62eefc79cedc827250b42e3a597726aa7c5abebd
Scott Johnson <mastertheclouds@gmail.com>
2022-04-07 08:56:33 -0500

    max - Using HostBinding to Bind to Host Properties

--- max/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ max/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,20 @@
+// remember me - using the Renderer to add styles
+// also using HostListener and HostBinding to listen to host events - mouseover, mouseleave
 import {
   Directive,
   OnInit,
   Renderer2,
   ElementRef,
   HostListener,
+  HostBinding,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
@@ -20,17 +25,20 @@ export class BetterHighlightDirective implements OnInit {
     );
   }
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = 'blue';
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    //   this.renderer.setStyle(
+    //     this.elRef.nativeElement,
+    //     'background-color',
+    //     'transparent'
+    //   );
+    // }
+    this.backgroundColor = 'transparent';
   }
 }
