35f5e21d910a94b125c3623cc98df2e9f1791eb3
Scott Johnson <mastertheclouds@gmail.com>
2022-04-07 10:19:28 -0500

    max - What Happens behind the Scenes on Structural Directives

--- max/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ max/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -16,17 +16,18 @@ import {
 export class BetterHighlightDirective implements OnInit {
   @Input() defaultColor: string = 'transparent';
   @Input() highlightColor: string = 'blue';
-  @HostBinding('style.backgroundColor') backgroundColor: string =
-    this.defaultColor;
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    this.backgroundColor = this.defaultColor;
+
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
   }
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(
