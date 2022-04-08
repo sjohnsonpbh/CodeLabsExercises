8f1f62dd5069dae2e287335e46e4ad1952f8b23e
Scott Johnson <mastertheclouds@gmail.com>
2022-04-08 10:55:52 -0500

    SideProjects/calendar-test2 in Angular

---
+++ SideProjects/calendar-test2/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
