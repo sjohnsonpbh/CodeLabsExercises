9de0f46a3ce3f1b58f8da7e6f9ecce9d089ded53
Scott Johnson <mastertheclouds@gmail.com>
2022-04-08 16:19:20 -0500

    max - Creating a Data Service

--- max/services-start/src/app/new-account/new-account.component.ts
+++ max/services-start/src/app/new-account/new-account.component.ts
@@ -2,24 +2,25 @@
 // see the constructor , the providers: in @Component and the logging.service file
 
 import { Component, EventEmitter, Output } from "@angular/core";
+import { AccountsService } from "../accounts.service";
 import { LoggingService } from "../logging.service";
 
 @Component({
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [LoggingService],
+  providers: [LoggingService, AccountsService],
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus,
-    });
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
