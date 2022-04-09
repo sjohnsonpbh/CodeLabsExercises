479cc4f2fea641c3e047fa0399097156783d79c6
Scott Johnson <mastertheclouds@gmail.com>
2022-04-09 08:28:41 -0500

    max - Injecting Services into Services

--- max/services-start/src/app/accounts.service.ts
+++ max/services-start/src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -14,6 +18,7 @@ export class AccountsService {
     },
   ];
 
+  constructor(private loggingService: LoggingService) {}
   addAccount(name: string, status: string) {
     this.accounts.push({ name: name, status: status });
   }
--- max/services-start/src/app/new-account/new-account.component.ts
+++ max/services-start/src/app/new-account/new-account.component.ts
@@ -9,7 +9,7 @@ import { LoggingService } from "../logging.service";
   selector: "app-new-account",
   templateUrl: "./new-account.component.html",
   styleUrls: ["./new-account.component.css"],
-  providers: [LoggingService],
+  // providers: [LoggingService],
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
@@ -21,6 +21,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
