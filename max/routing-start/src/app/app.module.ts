26ec90d4b91338dcd26fae6a538aa08a9c458ff3
Scott Johnson <mastertheclouds@gmail.com>
2022-04-12 09:48:43 -0500

    max - Practicing and some Common Gotchas

--- max/routing-start/src/app/app.module.ts
+++ max/routing-start/src/app/app.module.ts
@@ -17,9 +17,9 @@ const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "users", component: UsersComponent },
   { path: "users/:id/:name", component: UserComponent },
-  { path: "servers", component: ServerComponent },
-  { path: "servers/:id", component: ServersComponent },
-  { path: "servers/:id/:edit", component: EditServerComponent },
+  { path: "servers", component: ServersComponent },
+  { path: "servers/:id", component: ServerComponent },
+  { path: "servers/:id/edit", component: EditServerComponent },
 ];
 
 @NgModule({
