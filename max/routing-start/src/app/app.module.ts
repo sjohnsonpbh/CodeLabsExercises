e8ec69bdf00ec4b1fbed55646985c38a7014218f
Scott Johnson <mastertheclouds@gmail.com>
2022-04-12 13:57:25 -0500

    max -  Outsourcing the Route Configuration

--- max/routing-start/src/app/app.module.ts
+++ max/routing-start/src/app/app.module.ts
@@ -10,31 +10,9 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
-import { RouterModule, Routes } from "@angular/router";
+
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-
-// remember me - createing Routes
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [{ path: ":id/:name", component: UserComponent }],
-  },
-
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent },
-    ],
-  },
-  { path: "not-found", component: PageNotFoundComponent },
-  { path: "**", redirectTo: "not-found" }, // remember me - 404 error
-  // - ** is a wildcard
-  // this must be the last line of the list, they are evaluated top to bottom
-];
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -47,12 +25,24 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes), // remember me - loading the routes from the RouterModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// remember me - Redirection Path Matching
+// In our example, we didn't encounter any issues when we tried to redirect the user. But that's not always the case when adding redirections.
+
+// By default, Angular matches paths by prefix. That means, that the following route will match both /recipes and just /
+
+// { path: '', redirectTo: '/somewhere-else' }
+
+// Actually, Angular will give you an error here, because that's a common gotcha: This route will now ALWAYS redirect you! Why?
+
+// Since the default matching strategy is "prefix" , Angular checks if the path you entered in the URL does start with the path specified in the route. Of course every path starts with '' (Important: That's no whitespace, it's simply "nothing").
+
+// To fix this behavior, you need to change the matching strategy to "full" :
+
+// { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' }
+// Now, you only get redirected, if the full path is '' (so only if you got NO other content in your path in this example).
