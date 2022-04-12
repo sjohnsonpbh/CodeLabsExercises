6842b67adfa8fab2612afaf8e20b035f9f469b2b
Scott Johnson <mastertheclouds@gmail.com>
2022-04-12 14:35:49 -0500

    max - Protecting Routes with canActivate

---
+++ max/routing-start/src/app/auth.service.ts
@@ -0,0 +1,20 @@
+import { promise } from "protractor";
+
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated() {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+    return promise;
+  }
+  login() {
+    this.loggedIn = true;
+  }
+  logout() {
+    this.loggedIn = false;
+  }
+}
