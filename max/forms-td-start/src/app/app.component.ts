95b94d81e7954b8753b6880cef8f7864a50fbb1b
Scott Johnson <mastertheclouds@gmail.com>
2022-04-15 17:25:41 -0500

    max -     TD: Using the Form State

--- max/forms-td-start/src/app/app.component.ts
+++ max/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -6,11 +7,15 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  @ViewChild("f") signupForm: NgForm;
   suggestUserName() {
     const suggestedName = "Superuser";
   }
 
-  onSubmit(form: HTMLFormElement) {
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
