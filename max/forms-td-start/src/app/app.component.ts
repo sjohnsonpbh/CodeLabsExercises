2f2165f35e1c8df1aa2927fb3f5523f43358f1dd
Scott Johnson <mastertheclouds@gmail.com>
2022-04-16 08:37:44 -0500

    max -   TD: Set Default Values with ngModel Property Binding

--- max/forms-td-start/src/app/app.component.ts
+++ max/forms-td-start/src/app/app.component.ts
@@ -8,6 +8,9 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
+
+  defaultQuestion = "teacher";
+
   suggestUserName() {
     const suggestedName = "Superuser";
   }
