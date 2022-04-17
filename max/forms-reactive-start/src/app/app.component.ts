3aa98d8bd96fa1de8d15773cac712375d6500ed6
Scott Johnson <mastertheclouds@gmail.com>
2022-04-17 12:23:32 -0500

    max -   Reactive: Reacting to Status or Value Changes

--- max/forms-reactive-start/src/app/app.component.ts
+++ max/forms-reactive-start/src/app/app.component.ts
@@ -32,6 +32,9 @@ export class AppComponent implements OnInit {
       gender: new FormControl("male"),
       hobbies: new FormArray([]),
     });
+    // remember me - reacting to each change in the form
+    // this.signupForm.valueChanges.subscribe((value) => console.log(value));
+    this.signupForm.statusChanges.subscribe((value) => console.log(status));
   }
   onSubmit() {
     console.log(this.signupForm);
