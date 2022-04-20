94309414259710a81eb2d50f491575e3c8b461e4
Scott Johnson <mastertheclouds@gmail.com>
2022-04-20 15:10:37 -0500

    max -   Submitting the Recipe Edit Form

--- max/recipe-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ max/recipe-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeItemComponent } from '../recipe-list/recipe-item/recipe-item.component';
+import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -27,7 +29,16 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.recipeForm);
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']);
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
   }
 
   onAddIngredient() {
