f665420991b5ade53d0dbdd6e55450e3787d1d67
Scott Johnson <mastertheclouds@gmail.com>
2022-04-26 09:53:58 -0500

    max -   Using Types with the HttpClient

--- max/http-01-start/src/app/app.component.ts
+++ max/http-01-start/src/app/app.component.ts
@@ -1,6 +1,9 @@
+// remember me -   Using Types with the HttpClient
+// create posts and get posts back from google firebase
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { Post } from "./post.model";
 
 @Component({
   selector: "app-root",
@@ -16,11 +19,11 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     // console.log(postData);
     this.http
-      .post(
+      .post<{ name: string }>(
         "https://ng-complete-guide-9ac16-default-rtdb.firebaseio.com/posts.json",
         postData
       )
@@ -40,12 +43,12 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get(
+      .get<{ [key: string]: Post }>(
         "https://ng-complete-guide-9ac16-default-rtdb.firebaseio.com/posts.json"
       )
       .pipe(
         map((responseData) => {
-          const postsArray = [];
+          const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
               postsArray.push({ ...responseData[key], id: key });
