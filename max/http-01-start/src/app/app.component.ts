fba5820b845731cfdb0ca67ec638201e10dcd506
Scott Johnson <mastertheclouds@gmail.com>
2022-04-26 13:54:08 -0500

    max -   Sending a DELETE Request

--- max/http-01-start/src/app/app.component.ts
+++ max/http-01-start/src/app/app.component.ts
@@ -41,5 +41,8 @@ export class AppComponent implements OnInit {
 
   onClearPosts() {
     // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
   }
 }
--- max/http-01-start/src/app/posts.service.ts
+++ max/http-01-start/src/app/posts.service.ts
@@ -35,4 +35,10 @@ export class PostsService {
         })
       );
   }
+
+  deletePosts() {
+    return this.http.delete(
+      "https://ng-complete-guide-9ac16-default-rtdb.firebaseio.com/posts.json"
+    );
+  }
 }
