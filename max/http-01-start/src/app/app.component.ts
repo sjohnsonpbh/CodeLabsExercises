173da9abc0594e4c059e4c10a0cb8d769981b67e
Scott Johnson <mastertheclouds@gmail.com>
2022-04-26 10:05:29 -0500

    max -   Outputting Posts

--- max/http-01-start/src/app/app.component.ts
+++ max/http-01-start/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Post } from "./post.model";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -58,7 +58,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
-        console.log(posts);
+        this.loadedPosts = posts;
       });
   }
 }
