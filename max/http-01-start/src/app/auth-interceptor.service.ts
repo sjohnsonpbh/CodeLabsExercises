fa8fc63dd9f373be380ef19264696ca018e95e66
Scott Johnson <mastertheclouds@gmail.com>
2022-04-27 09:34:31 -0500

    max -   Response Interceptors

--- max/http-01-start/src/app/auth-interceptor.service.ts
+++ max/http-01-start/src/app/auth-interceptor.service.ts
@@ -1,8 +1,10 @@
 import {
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 // remember me - Interceptors - adds a header to every request leaving app
 export class AuthInterceptorService implements HttpInterceptor {
@@ -12,6 +14,14 @@ export class AuthInterceptorService implements HttpInterceptor {
     const modifiedRequest = req.clone({
       headers: req.headers.append("Auth", "xyz"),
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log("Response arrived, body data: ");
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
