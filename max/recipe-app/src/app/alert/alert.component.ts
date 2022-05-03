e738d76296af399954eaa2a03243ea7f67598fdc
Scott Johnson <mastertheclouds@gmail.com>
2022-05-03 08:50:03 -0500

    max -   Using ngIf

--- max/recipe-app/src/app/alert/alert.component.ts
+++ max/recipe-app/src/app/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +6,12 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./alert.component.css'],
 })
 export class AlertComponent {
-  @Input() message: string;
+  @Input() message: string; // remember me - property binding to
+  // send error = message
+
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
 }
