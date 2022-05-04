d6d6932bf446bb5c9dba644dc186b55cab2a1570
Scott Johnson <mastertheclouds@gmail.com>
2022-05-04 09:07:39 -0500

    max -   Understanding Shared Modules

--- max/recipe-app/src/app/app.module.ts
+++ max/recipe-app/src/app/app.module.ts
@@ -5,31 +5,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropDownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner';
 import { AuthInterceptorService } from './auth/auth.interceptor.service';
 import { AlertComponent } from './alert/alert.component';
-import { PlaceHolderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 // remember me - @NgModule declarations array contains all the components,
 // directives, and custom pipes for our application
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    DropDownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceHolderDirective,
-  ],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   // this allows you to add other modules with additional capabilities
   imports: [
     BrowserModule,
@@ -39,6 +29,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
+    SharedModule,
   ],
   // remember me - Providers are added here, but can also be used in other
   // files by using { providedIn: 'root' }
@@ -54,7 +45,5 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
   ],
   // remember me - this is needed to startup the app with a main component
   bootstrap: [AppComponent],
-  // the below is not needed in Angular 9+
-  entryComponents: [AlertComponent],
 })
 export class AppModule {}
