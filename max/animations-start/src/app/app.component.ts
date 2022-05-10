87d58c83b08f24bb4315353bab03d2d2973243ce
Scott Johnson <mastertheclouds@gmail.com>
2022-05-10 14:27:32 -0500

    max -  The "void" State

--- max/animations-start/src/app/app.component.ts
+++ max/animations-start/src/app/app.component.ts
@@ -27,19 +27,87 @@ import { Component } from "@angular/core";
           transform: "translateX(100px)",
         })
       ),
-      transition("normal => highlighted", animate(1300)),
+      transition("normal <=> highlighted", animate(1300)),
+      // transition("highlighted => normal", animate(800)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0) scale(1)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0) scale(0.5)",
+        })
+      ),
+      transition("normal => highlighted", animate(300)),
       transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", [
+        style({
+          "background-color": "orange",
+        }),
+        animate(
+          1000,
+          style({
+            borderRadius: "50px",
+          })
+        ),
+        animate(2300),
+      ]),
+    ]),
+    trigger("list1", [
+      state(
+        "in",
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        })
+      ),
+      transition("void => *", [
+        style({
+          opacity: 0,
+          transform: "translateX(-100)",
+        }),
+        animate(2300),
+      ]),
+      transition("* => void", [
+        animate(2300),
+        style({
+          transform: "translateX(100px)",
+          opacity: 0,
+        }),
+      ]),
+      // transition("highlighted => normal", animate(800)),
     ]),
   ],
 })
 export class AppComponent {
   state = "normal";
+  wildState = "normal";
   list = ["Milk", "Sugar", "Bread"];
 
   onAnimate() {
     this.state == "normal"
       ? (this.state = "highlighted")
       : (this.state = "normal");
+    this.wildState == "normal"
+      ? (this.wildState = "highlighted")
+      : (this.wildState = "normal");
+  }
+  onShrink() {
+    this.wildState = "shrunken";
   }
   onAdd(item) {
     this.list.push(item);
