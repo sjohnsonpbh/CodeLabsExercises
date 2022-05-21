73f29542cfa0e45c4fd7c7a714534c4b0e4218c0
Scott Johnson <mastertheclouds@gmail.com>
2022-05-21 09:28:50 -0500

    Tour of Heroes - up to Add Navigation

--- SideProjects/angular-tour-of-heroes_old/README.md
+++ SideProjects/angular-tour-of-heroes_old/README.md
@@ -1,6 +1,6 @@
 # AngularTourOfHeroes
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.0.
 
 ## Development server
 
--- SideProjects/angular-tour-of-heroes_old/src/app/app.module.ts
+++ SideProjects/angular-tour-of-heroes_old/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
